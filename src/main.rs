6ed9bb92b7d51b15071cdde79b94f79f31499c3c
Konstantinos Athanasiou <dinosath0@gmail.com>
2023-08-30 18:57:53 +0300

    create kubernetes job upon rest call

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,122 @@
-use k8s_openapi::api::core::v1::{Pod, PersistentVolume};
+use k8s_openapi::api::batch::v1::Job;
+use k8s_openapi::api::core::v1::{PersistentVolume, Pod};
 
 use kube::{
     api::{Api, DeleteParams, ListParams, Patch, PatchParams, PostParams, ResourceExt},
-    runtime::wait::{await_condition, conditions::is_pod_running},
+    runtime::wait::{await_condition, conditions, conditions::is_pod_running},
     Client,
 };
+use tracing::info;
+
+use axum::{
+    extract::{rejection::FormRejection, Form, FromRequest},
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    Json,
+    routing::{get,post},
+    Router,
+};
+
+use std::net::SocketAddr;
+use validator::Validate;
+
+use serde::{Deserialize, Serialize};
 
 
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JobBuilderRequest {
+    pub openapis: Vec<Openapi>,
+    pub json_schemas: Vec<JsonSchema>,
+    pub template_url: String,
+}
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Openapi {
+    pub url: String,
+    pub authentication: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct JsonSchema {
+    pub url: String,
+    pub authentication: String,
+}
+
+async fn create_job(Json(payload): Json<JobBuilderRequest>,) -> Result<(), AppError> {
     let client = Client::try_default().await?;
+    
+    let jobs: Api<Job> = Api::default_namespaced(client);
+    let name = "openapi-generator-";
+    
+    let data = serde_json::from_value(serde_json::json!({
+        "apiVersion": "batch/v1",
+        "kind": "Job",
+        "metadata": {
+            "generateName": name,
+        },
+        "spec": {
+            "template": {
+                "metadata": {
+                    "name": "empty-job-pod"
+                },
+                "spec": {
+                    "containers": [{
+                        "name": "empty",
+                        "image": "alpine:latest"
+                    }],
+                    "restartPolicy": "Never",
+                }
+            }
+        }
+    }))?;
+    info!("Creating job");
 
+    jobs.create(&PostParams::default(), &data).await?;
+    info!("Waiting for job to complete");
+    let cond = await_condition(jobs.clone(), name, conditions::is_job_completed());
+    let _ = tokio::time::timeout(std::time::Duration::from_secs(60), cond).await?;
 
-    let pvs: Api<PersistentVolume> = Api::all(client);
-    for pv in pvs.list(&ListParams::default().fields((""))).await? {
-        println!("Found Pv: {}", pv.name_any());
-    }
-
-    // let pods: Api<Pod> = Api::namespaced(client, "apps");
-    // let lp = ListParams::default().labels("app=blog"); // for this app only
-    // for p in pods.list(&lp).await? {
-    //     println!("Found Pod: {}", p.name_any());
-    // }
+    info!("Cleaning up job record");
+    jobs.delete(name, &DeleteParams::background()).await?;
 
     Ok(())
+}
+
+struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let app = Router::new()
+        .route("/schematools/build", post(create_job));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 
 }

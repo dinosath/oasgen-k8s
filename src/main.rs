4010bc469be3495f5ffada6fcae0c6c3493b3259
Konstantinos Athanasiou <dinosath0@gmail.com>
2023-08-31 21:53:05 +0300

    add validation trait in request body and create schema tools job

--- src/main.rs
+++ src/main.rs
@@ -12,60 +12,127 @@ use axum::{
     extract::{rejection::FormRejection, Form, FromRequest},
     http::StatusCode,
     response::{Html, IntoResponse, Response},
-    Json,
-    routing::{get,post},
-    Router,
+    routing::{get, post},
+    Json, Router,
 };
 
 use std::net::SocketAddr;
-use validator::Validate;
+use validator::{Validate,ValidationError, ValidationErrors};
 
 use serde::{Deserialize, Serialize};
-
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct JobBuilderRequest {
-    pub openapis: Vec<Openapi>,
-    pub json_schemas: Vec<JsonSchema>,
-    pub template_url: String,
+    pub openapis: Vec<Schema>,
+    pub json_schemas: Vec<Schema>,
+    pub template: Template,
+}
+
+impl Validate for JobBuilderRequest {
+    fn validate(&self) -> Result<(), validator::ValidationErrors> {
+        let openapi_is_empty = self.openapis.len() == 0;
+        if openapi_is_empty && self.json_schemas.len()==0 {
+            let mut errors = ValidationErrors::new();
+            let erroneous_field = if openapi_is_empty {"openapis"} else {"jsonSchemas"};
+            errors.add(erroneous_field,ValidationError::new("must provide at least one openapi url or one jsonschema url"));
+            return Err(errors);
+        }
+        Ok(())    
+    }
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Openapi {
+pub struct Schema {
     pub url: String,
-    pub authentication: String,
+    pub authentication: Option<String>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct JsonSchema {
+pub struct Template {
     pub url: String,
-    pub authentication: String,
+    pub tag: Tag,
 }
 
-async fn create_job(Json(payload): Json<JobBuilderRequest>,) -> Result<(), AppError> {
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Tag {
+    pub name: String,
+    #[serde(rename = "type")]
+    pub tag_type: Type,
+}
+
+impl Default for Tag {
+    fn default() -> Self {
+        Tag {
+            name: "master".to_owned(),
+            tag_type: Type::default(),
+        }
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum Type {
+    #[default]
+    Branch,
+    Tag,
+    Revision,
+}
+
+async fn create_job(Json(payload): Json<JobBuilderRequest>) -> Result<(), AppError> {
     let client = Client::try_default().await?;
-    
+
     let jobs: Api<Job> = Api::default_namespaced(client);
-    let name = "openapi-generator-";
-    
+    let name = "oasgen-";
+
     let data = serde_json::from_value(serde_json::json!({
         "apiVersion": "batch/v1",
         "kind": "Job",
         "metadata": {
             "generateName": name,
+            "labels": {
+                "app" : "oasgen"
+            }
         },
         "spec": {
             "template": {
                 "metadata": {
-                    "name": "empty-job-pod"
+                    "name": "schema-tools"
                 },
                 "spec": {
+                    "volumes":[
+                        {
+                            "name": "shared-data",
+                            "emptyDir": {}
+                        }
+                    ],
+                    "initContainers": [{
+                        "name": "schema-tools",
+                        "image": "ghcr.io/dinosath/schema-tools:master",
+                        "args":["chain","-vvvv","-c",format!("registry add common https://github.com/dinosath/schema-tools-templates.git --branch master"),"-c","validate openapi https://petstore3.swagger.io/api/v3/openapi.json","-c","codegen openapi - --template common::rust/server-axum/ --target-dir /data/ -o namespace=client1 -o clientName=Client1"
+                        ],
+                        "volumeMounts":[
+                            {
+                                "name": "shared-data",
+                                "mountPath": "/data"
+                            }
+                          ]
+                    }],
                     "containers": [{
-                        "name": "empty",
-                        "image": "alpine:latest"
+                        "name": "alpine",
+                        "image": "alpine:latest",
+                        "command": ["/bin/sh"],
+                        "args":["ls","/data"],
+                        "volumeMounts":[
+                            {
+                                "name": "shared-data",
+                                "mountPath": "/data"
+                            }
+                          ]
+
                     }],
                     "restartPolicy": "Never",
                 }
@@ -109,8 +176,7 @@ where
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    let app = Router::new()
-        .route("/schematools/build", post(create_job));
+    let app = Router::new().route("/schematools/build", post(create_job));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
@@ -118,5 +184,4 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
-
 }
